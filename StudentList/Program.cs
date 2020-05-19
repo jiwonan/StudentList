f14046a37f61fc12be1d6efb1d35588dd4af6441
jiwonan <s2018s31@e-mirim.hs.kr>
2020-05-19 10:58:54 +0900

    List 생성, 초기화

--- StudentList/Program.cs
+++ StudentList/Program.cs
@@ -8,8 +8,26 @@ namespace StudentList
 {
     class Program
     {
+        class Teacher
+        {
+            public string name;
+            public String major;
+        }
+
         static void Main(string[] args)
         {
+            List<Teacher> list = new List<Teacher>();
+            list.Add(new Teacher() { name = "함기훈", major = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "박성래", major = "수학" });
+            list.Add(new Teacher() { name = "민주리", major = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "박지우", major = "정보컴퓨터" });
+            list.Add(new Teacher() { name = "김보경", major = "가정" });
+            list.Add(new Teacher() { name = "신혜정", major = "정보컴퓨터" });
+
+            foreach(var item in list)
+            {
+                Console.WriteLine(item.name+" : "+item.major);
+            }
         }
     }
 }
---
+++ StudentList/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList
+{
+    class Student
+    {
+        // 변수만 가지고 있는 클래스 : 모델클래스
+        public string id;
+        public string name;
+        public int grade;
+        public string major;
+        public DateTime birthday;
+    }
+}
